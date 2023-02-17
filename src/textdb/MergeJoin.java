05ee41276006c0f45436feee42783149b6612713
Sunvat <sunvat14@gmail.com>
2023-02-17 14:52:48 -0800

    completed both methods

--- src/textdb/MergeJoin.java
+++ src/textdb/MergeJoin.java
@@ -9,6 +9,8 @@ public class MergeJoin extends Operator {
 	private int MERGE_BUFFER_SIZE = 10000; // The number of tuples that can be buffered with the same key.
 	private EquiJoinPredicate pred; // A equi-join comparison class that can handle 1 or more attributes
 	// Iterator state variables
+	private Tuple tupleLeft;
+	private Tuple tupleRight;
 
 	public MergeJoin(Operator[] in, EquiJoinPredicate p) {
 		super(in, 0, 0);
@@ -25,29 +27,23 @@ public class MergeJoin extends Operator {
 		setOutputRelation(out);
 
 		// TODO: YOUR SETUP CODE HERE
-		Tuple leftBuff = input[0].next();
-		Tuple rightBuff = input[1].next();	
+		tupleLeft= input[0].next();
 	}
 
 	public Tuple next() throws IOException {
-
-		// TODO: YOUR SETUP CODE HERE
-		Tuple leftBuff = input[0].next();
-		Tuple rightBuff = input[1].next();
-		// check if we have moved through all the tuples in a relation
-		while(leftBuff != null && rightBuff !=null){
-			
-		if(pred.isEqual(leftBuff, rightBuff)){
-		outputJoinTuple(leftBuff, rightBuff);
-		return outputJoinTuple(leftBuff, rightBuff);
-		}
-		else{
-			return null;
-		}
-	}
-		return rightBuff;// to fix error should not return right tuple
-		
-	}
+        // TODO: YOUR SETUP CODE HERE
+        // check if we have moved through all the tuples in a relation
+        while(tupleLeft != null){
+            tupleRight = input[1].next();
+            while(tupleRight !=null){
+                if(pred.isEqual(tupleLeft, tupleRight))
+                  outputJoinTuple(tupleLeft, tupleRight);
+            }
+			input[1].init();
+			tupleRight = input[1].next();
+        }
+        return null;
+}
 
 	public void close() throws IOException {
 		super.close();
